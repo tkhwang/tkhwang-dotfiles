7af3ec6a492fa7e3a208a0ad995f4f584e69c00e
tkhwang <tkhwang@gmail.com>
2014-12-07 14:08:22 +0900

    [+] ascii-table added.

--- personal/tkhwang-dotemacs.el
+++ personal/tkhwang-dotemacs.el
@@ -115,6 +115,29 @@
 (add-hook 'python-mode-hook 'jedi:setup)
 (setq jedi:complete-on-dot t)
 
+;; -----------------------------------------------------------------------------
+;; ascii-table
+;; http://www.emacswiki.org/AsciiTable
+;; -----------------------------------------------------------------------------
+(autoload 'ascii-table "ascii-table" "Show ASCII table." t)
+(defun ascii-table ()
+  "Display basic ASCII table (0 thru 128)."
+  (interactive)
+  (setq buffer-read-only nil)        ;; Not need to edit the content, just read mode (added)
+  (local-set-key "q" 'bury-buffer)   ;; Nice to have the option to bury the buffer (added)
+  (switch-to-buffer "*ASCII*")
+  (erase-buffer)
+  (save-excursion (let ((i -1))
+                    (insert "ASCII characters 0 thru 127.\n\n")
+                    (insert " Hex  Dec  Char|  Hex  Dec  Char|  Hex  Dec  Char|  Hex  Dec  Char\n")
+                    (while (< i 31)
+                      (insert (format "%4x %4d %4s | %4x %4d %4s | %4x %4d %4s | %4x %4d %4s\n"
+                                      (setq i (+ 1  i)) i (single-key-description i)
+                                      (setq i (+ 32 i)) i (single-key-description i)
+                                      (setq i (+ 32 i)) i (single-key-description i)
+                                      (setq i (+ 32 i)) i (single-key-description i)))
+                      (setq i (- i 96))))))
+
 ;; ----------------------------------------------------------------------------
 (provide 'tkhwang-dotemacs) ; --------------------------------------------- END
 ;; ----------------------------------------------------------------------------
